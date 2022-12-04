de3a307ea5b9fe4ac9775e2922e952f0d4d79069
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2022-12-04 13:47:52 +0000

    2020 day 6 part 2

--- 2020/day06/index.ts
+++ 2020/day06/index.ts
@@ -5,19 +5,41 @@ const rawInput: string = require('fs')
 const rows = rawInput.split("\n");
 rows.push("");
 
-let runningTotal = 0;
+let runningTotalGroupAnswered = 0;
+let runningTotalGroupAnsweredByEveryone = 0;
 let currentGroupAnswered: Set<string> = new Set();
+let currentGroupAnsweredByEveryone: Set<string> | null = null;
 
 for (let row of rows) {
     if (row.length === 0) {
-        runningTotal += currentGroupAnswered.size;
+        runningTotalGroupAnswered += currentGroupAnswered.size;
         currentGroupAnswered = new Set();
+
+        if (currentGroupAnsweredByEveryone !== null) {
+            runningTotalGroupAnsweredByEveryone += currentGroupAnsweredByEveryone.size;
+            currentGroupAnsweredByEveryone = null;
+        }
     } else {
         const answers = row.split("");
+
         answers.forEach(answer => currentGroupAnswered.add(answer));
+
+        if (currentGroupAnsweredByEveryone === null) {
+            currentGroupAnsweredByEveryone = new Set(answers);
+        } else {
+            const currentAnswers = new Set(answers);
+
+            Array.from(currentGroupAnsweredByEveryone.values())
+                .forEach(answer => {
+                    if (!currentAnswers.has(answer)) {
+                        currentGroupAnsweredByEveryone.delete(answer);
+                    }
+                });
+        }
     }
 }
 
-console.log("Part 1 = " + runningTotal);
+console.log("Part 1 = " + runningTotalGroupAnswered);
+console.log("Part 2 = " + runningTotalGroupAnsweredByEveryone);
 
 export {};
