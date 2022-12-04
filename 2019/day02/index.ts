18bf8aedbf04f28c4398ce0082c7b8a46cf2aa60
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2022-12-04 13:47:49 +0000

    Solve 2019 day 2 part 2

--- 2019/day02/index.ts
+++ 2019/day02/index.ts
@@ -1,14 +1,7 @@
-const memory: number[] = require('fs')
+const program: number[] = require('fs')
     .readFileSync('2019/day02/input.txt').toString()
     .split(",")
     .map((str: string) => Number.parseInt(str));
-
-// BEFORE RUNNING replace position 1 with the value 12 and replace position 2 with the value 2.
-memory[1] = 12;
-memory[2] = 2;
-
-let instructionPointer = 0;
-let opCode: number = memory[0];
 
 const performOperation = (opCode: number, parameterA: number, parameterB: number): number => {
     switch (opCode) {
@@ -21,15 +14,56 @@ const performOperation = (opCode: number, parameterA: number, parameterB: number
     throw new Error("Unknown opCode: " + opCode);
 }
 
-while (opCode !== 99) {
-    const parameterAddressA = memory[instructionPointer + 1];
-    const parameterAddressB = memory[instructionPointer + 2];
-    const outputAddress = memory[instructionPointer + 3];
+const resetAndExecute = (noun = 12, verb = 2) => {
+    const memory: number[] = program.map(n => n);
 
-    memory[outputAddress] = performOperation(opCode, memory[parameterAddressA], memory[parameterAddressB]);
+    // BEFORE RUNNING replace position 1 with the noun and replace position 2 with the verb.
+    memory[1] = noun;
+    memory[2] = verb;
 
-    instructionPointer += 4;
-    opCode = memory[instructionPointer];
+    let instructionPointer = 0;
+    let opCode: number = memory[0];
+
+    while (opCode !== 99) {
+        const parameterAddressA = memory[instructionPointer + 1];
+        const parameterAddressB = memory[instructionPointer + 2];
+        const outputAddress = memory[instructionPointer + 3];
+
+        memory[outputAddress] = performOperation(opCode, memory[parameterAddressA], memory[parameterAddressB]);
+
+        instructionPointer += 4;
+        opCode = memory[instructionPointer];
+    }
+
+    return memory[0];
 }
 
-console.log("Part 1: " + memory[0]);
+console.log("Part 1: " + resetAndExecute());
+
+class FoundResult extends Error {
+    constructor(private noun: number, private verb: number) {
+        super(`Found result. noun=${noun}, verb=${verb}`);
+    }
+
+    get answer(): number {
+        return 100 * this.noun + this.verb;
+    }
+}
+
+try {
+    for (let noun = 0; noun <= 99; ++noun) {
+        for (let verb = 0; verb <= 99; ++verb) {
+            if (19690720 === resetAndExecute(noun, verb)) {
+                throw new FoundResult(noun, verb);
+            }
+        }
+    }
+
+    throw new Error("Did not find expected result");
+} catch (e) {
+    if (e instanceof FoundResult) {
+        console.log("Part 2: " + e.answer);
+    } else {
+        throw e;
+    }
+}
