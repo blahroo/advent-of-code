cc3892206f89b30c6f8a827d881d9c12bf424f90
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2022-12-04 13:47:52 +0000

    2020 day 6 part 1

--- 2020/day06/index.ts
+++ 2020/day06/index.ts
@@ -3,5 +3,21 @@ const rawInput: string = require('fs')
     .toString()
     .replace(/[\r]/g, '');
 const rows = rawInput.split("\n");
+rows.push("");
+
+let runningTotal = 0;
+let currentGroupAnswered: Set<string> = new Set();
+
+for (let row of rows) {
+    if (row.length === 0) {
+        runningTotal += currentGroupAnswered.size;
+        currentGroupAnswered = new Set();
+    } else {
+        const answers = row.split("");
+        answers.forEach(answer => currentGroupAnswered.add(answer));
+    }
+}
+
+console.log("Part 1 = " + runningTotal);
 
 export {};
