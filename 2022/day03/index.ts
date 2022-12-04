371e6df8d5b3df80cc30d286e7fb8803982bdcb6
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2022-12-04 13:47:53 +0000

    2022 day 03 part 2

--- 2022/day03/index.ts
+++ 2022/day03/index.ts
@@ -1,5 +1,8 @@
 import { Day03Input } from "./input";
 
+const LOWER_CASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz".split("");
+const UPPER_CASE_ALPHABET = LOWER_CASE_ALPHABET.map((a) => a.toUpperCase());
+
 const findItemInBothCompartments = (line: string) => {
   const characters = line.split("");
   const mid = characters.length / 2;
@@ -17,9 +20,6 @@ const findItemInBothCompartments = (line: string) => {
 
   return ret;
 };
-
-const LOWER_CASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz".split("");
-const UPPER_CASE_ALPHABET = LOWER_CASE_ALPHABET.map((a) => a.toUpperCase());
 
 const getItemPriority = (char: string) => {
   if (char.length !== 1) {
@@ -39,15 +39,40 @@ const getItemPriority = (char: string) => {
   return upperIndex + 27;
 };
 
-let totalPriorities = 0;
+let totalPrioritiesPart1 = 0;
 
 for (const line of Day03Input) {
   const dupe = findItemInBothCompartments(line);
   const priority = getItemPriority(dupe);
 
-  totalPriorities += priority;
+  totalPrioritiesPart1 += priority;
+}
+
+const findCommonInAll = (a: string, b: string, c: string) => {
+  const elf1 = new Set(a.split(""));
+  const elf2 = new Set(b.split(""));
+  const elf3 = new Set(c.split(""));
+
+  for (const item of elf1) {
+    if (elf2.has(item) && elf3.has(item)) {
+      return item;
+    }
+  }
+
+  throw new Error("Did not find a common item");
+};
+
+let totalPrioritiesPart2 = 0;
+for (let groupNo = 0; groupNo < Day03Input.length / 3; ++groupNo) {
+  const offset = groupNo * 3;
+  const elf1 = Day03Input[offset];
+  const elf2 = Day03Input[offset + 1];
+  const elf3 = Day03Input[offset + 2];
+  const commonItem = findCommonInAll(elf1, elf2, elf3);
+  totalPrioritiesPart2 += getItemPriority(commonItem);
 }
 
 console.log({
-  totalPriorities,
+  totalPrioritiesPart1,
+  totalPrioritiesPart2,
 });
