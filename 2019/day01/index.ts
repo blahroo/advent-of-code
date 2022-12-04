6e200e11b645f2651e00560daee57f637cb5d986
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2022-12-04 13:47:49 +0000

    Solve 2019 day 1

---
+++ 2019/day01/index.ts
@@ -0,0 +1,31 @@
+const moduleWeights = require('fs')
+    .readFileSync('2019/day01/input.txt').toString()
+    .split("\n")
+    .map((str: string) => Number.parseInt(str));
+
+const calculateFuelFoMass = (mass: number): number => Math.floor(mass / 3) - 2;
+const sumFuels = (runningTotal: number, moduleWeight: number) => runningTotal + moduleWeight;
+
+const totalRequiredFuel = moduleWeights
+    .map(calculateFuelFoMass)
+    .reduce(sumFuels, 0);
+
+console.log("Part 1: " + totalRequiredFuel);
+
+const calculateTotalFuelWithFuelToLiftFuelForMass = (moduleMass: number): number => {
+    let totalFuelRequired = calculateFuelFoMass(moduleMass);
+    let fuelToLiftLastFuel = calculateFuelFoMass(totalFuelRequired)
+
+    while (fuelToLiftLastFuel > 0) {
+        totalFuelRequired += fuelToLiftLastFuel;
+        fuelToLiftLastFuel = calculateFuelFoMass(fuelToLiftLastFuel);
+    }
+
+    return totalFuelRequired;
+}
+
+const totalFuelWithFuelConsideration = moduleWeights
+    .map(calculateTotalFuelWithFuelToLiftFuelForMass)
+    .reduce(sumFuels, 0);
+
+console.log("Part 2: " + totalFuelWithFuelConsideration);
