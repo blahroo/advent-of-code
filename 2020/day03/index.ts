f9e26460bf44b2fdf63f48024f92f957e19903fc
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2022-12-04 13:47:52 +0000

    2020 day 3 part 2

--- 2020/day03/index.ts
+++ 2020/day03/index.ts
@@ -1,26 +1,39 @@
 const rawInput: string = require('fs')
     .readFileSync('2020/day03/input.txt')
-//    .readFileSync('2020/day03/exampleInput.txt')
     .toString()
 
 const rows = rawInput.split("\n");
 const rowWidth = rows[0].length;
 
-let treesSeen = 0;
-let xPos = 0;
+function countTreesUsingRoute(xOffset: number, yOffset: number) {
+    let xPos = 0;
+    let treesSeen = 0;
 
-rows.forEach((row, yPos) => {
-
-    const charAtThisPoint = row.charAt(xPos);
-    console.log(`x: ${xPos}, y: ${yPos} = ${charAtThisPoint}`);
-
-    if (charAtThisPoint === '#') {
-        ++treesSeen;
+    for (let yPos = 0; yPos < rows.length; yPos += yOffset) {
+        const charAtPosition = rows[yPos].charAt(xPos);
+        if (charAtPosition === "#") {
+            ++treesSeen;
+        }
+        xPos = (xPos + xOffset) % (rowWidth - 1);
     }
 
-    xPos = (xPos + 3) % (rowWidth - 1);
-});
+    return treesSeen;
+}
 
-console.log(`Saw ${treesSeen} trees`);
+const routeR3D1 = countTreesUsingRoute(3, 1);
+console.log(`Part 1: Saw ${routeR3D1} trees`); // 7 demo, 237 part 1
+
+const routeR1D1 = countTreesUsingRoute(1, 1);
+const routeR5D1 = countTreesUsingRoute(5, 1);
+const routeR7D1 = countTreesUsingRoute(7, 1);
+const routeR1D2 = countTreesUsingRoute(1, 2);
+
+console.log("Right 1, down 1. " + routeR1D1);
+console.log("Right 3, down 1. " + routeR3D1);
+console.log("Right 5, down 1. " + routeR5D1);
+console.log("Right 7, down 1. " + routeR7D1);
+console.log("Right 1, down 2. " + routeR1D2);
+
+console.log("Part 2: Trees seen = " + (routeR1D1 * routeR3D1 * routeR5D1 * routeR7D1 * routeR1D2)); // 2106818610
 
 export {};
