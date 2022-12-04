728efade1dc194f72d90a76b13b63a2443c6e69e
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2022-12-04 13:47:51 +0000

    Refactoring

--- 2019/day05/index.ts
+++ 2019/day05/index.ts
@@ -14,9 +14,22 @@ const PARAMETER_MODE_POSITION = 0;
 const PARAMETER_MODE_IMMEDIATE = 1;
 type ParameterMode = 0 | 1;
 
-const takeNextInput = () => {
-    return 1;
+class OperationDetails {
+    constructor(public totalInputs: number, public totalOutputs: number) { }
+
+    get totalSize() {
+        return 1 + this.totalInputs + this.totalOutputs;
+    }
 }
+
+const operationDetailLookup: {[key: number]: OperationDetails} = {};
+operationDetailLookup[OPERATION_ADD] = new OperationDetails(2, 1);
+operationDetailLookup[OPERATION_MULTIPLY] = new OperationDetails(2, 1);
+operationDetailLookup[OPERATION_TAKE_INPUT] = new OperationDetails(0, 1);
+operationDetailLookup[OPERATION_PRINT] = new OperationDetails(1, 0)
+operationDetailLookup[OPERATION_TERMINATE] = new OperationDetails(0, 0);
+
+const takeNextInput = (): number => 1;
 
 const safeGetParameter = (index: number, parameters: number[]): number => {
     if (parameters.length <= index) {
@@ -28,31 +41,16 @@ const safeGetParameter = (index: number, parameters: number[]): number => {
 
 const performOperation = (opCode: Operation, parameters: number[]): number | null => {
     switch (opCode) {
-        case OPERATION_ADD: {
-            const p1 = safeGetParameter(0, parameters);
-            const p2 = safeGetParameter(1, parameters);
-            const output = p1 + p2;
-
-            console.log(`ADD ${p1} and ${p2} = ${output}`);
-
-            return output;
-        }
-        case OPERATION_MULTIPLY: {
-            const p1 = safeGetParameter(0, parameters);
-            const p2 = safeGetParameter(1, parameters);
-            const output = p1 * p2;
-
-            console.log(`MULTIPLY ${p1} and ${p2} = ${output}`);
-
-            return output;
-        }
+        case OPERATION_ADD:
+            return safeGetParameter(0, parameters) + safeGetParameter(1, parameters);
+        case OPERATION_MULTIPLY:
+            return safeGetParameter(0, parameters) * safeGetParameter(1, parameters);
         case OPERATION_TAKE_INPUT:
-            console.log("Taking input")
             return takeNextInput();
         case OPERATION_PRINT: {
-            console.log(`Output: ${safeGetParameter(0, parameters)}`);
+            console.log(safeGetParameter(0, parameters));
             return null;
-        };
+        }
     }
 
     throw new Error("Unknown opCode: " + opCode);
@@ -61,13 +59,8 @@ const performOperation = (opCode: Operation, parameters: number[]): number | nul
 const extractOperation = (opCode: number): Operation => {
     const output = opCode % 100;
 
-    switch (output) {
-        case OPERATION_ADD:
-        case OPERATION_MULTIPLY:
-        case OPERATION_TAKE_INPUT:
-        case OPERATION_PRINT:
-        case OPERATION_TERMINATE:
-            return output;
+    if (operationDetailLookup[output]) {
+        return output as Operation;
     }
 
     throw new Error("Unknown operation: " + output + " for opcode: " + opCode);
@@ -100,39 +93,18 @@ const getParameterModesForOpCode = (opCode: number): ParameterMode[] => {
 };
 
 const getOperationSize = (operation: Operation): number => {
-    switch (operation) {
-        case OPERATION_ADD:
-        case OPERATION_MULTIPLY:
-            // Base size, 2 inputs, 1 destination
-            return 4;
-        case OPERATION_TAKE_INPUT:
-            // Base size, 0 inputs, 1 destination
-            return 2;
-        case OPERATION_PRINT:
-            // Base size, 1 input, 0 destination
-            return 2;
-        case OPERATION_TERMINATE:
-            // Base size, 0 input, 0 destination
-            return 1;
-        default:
-            throw new Error("Unknown operation: " + operation);
+    if (operationDetailLookup[operation]) {
+        return operationDetailLookup[operation].totalSize;
     }
+    throw new Error("Unknown operation: " + operation);
 };
 
 const getTotalParameters = (operation: Operation): number => {
-    switch (operation) {
-        case OPERATION_ADD:
-        case OPERATION_MULTIPLY:
-            return 2;
-        case OPERATION_PRINT:
-            return 1;
-        case OPERATION_TERMINATE:
-        case OPERATION_TAKE_INPUT:
-            return 0;
-        default:
-            throw new Error("Unknown operation: " + operation);
+    if (operationDetailLookup[operation]) {
+        return operationDetailLookup[operation].totalInputs;
     }
-}
+    throw new Error("Unknown operation: " + operation);
+};
 
 const executeProgram = (rawProgram: number[]) => {
     const memory: number[] = rawProgram.map(n => n);
@@ -140,19 +112,11 @@ const executeProgram = (rawProgram: number[]) => {
     let instructionPointer = 0;
     let opCode: number = memory[0];
 
-    let cycle = 0;
-
     while (opCode !== OPERATION_TERMINATE) {
-
-        console.log(`\n\n***Performing cycle ${++cycle} ***`);
-
-
         const parameterModes = getParameterModesForOpCode(opCode);
         const operation = extractOperation(opCode);
         const operationSize = getOperationSize(operation);
-
         const totalParameters = getTotalParameters(operation);
-        console.log(`Expecting ${totalParameters} parameter${totalParameters == 1 ? "." : "s."}`);
 
         const parameters: number[] = [];
         for (let i = 0; i < totalParameters; ++i) {
@@ -167,7 +131,6 @@ const executeProgram = (rawProgram: number[]) => {
         const operationOutput = performOperation(operation, parameters);
         if (operationOutput !== null) {
             const outputAddress = memory[instructionPointer + totalParameters + 1];
-            console.log(`Storing ${operationOutput} to address ${outputAddress}`);
             memory[outputAddress] = operationOutput;
         }
 
@@ -178,8 +141,6 @@ const executeProgram = (rawProgram: number[]) => {
     return memory[0];
 };
 
-console.log("Output = " + executeProgram(day5Program));
-
-// 152702 Too low
+console.log("Program Output Code = " + executeProgram(day5Program));
 
 export {}
