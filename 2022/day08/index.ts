87833225b290286d8ee66cffc8243b4834b079d6
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2022-12-08 17:55:35 +0000

    2022 day 8 part 2

--- 2022/day08/index.ts
+++ 2022/day08/index.ts
@@ -1,8 +1,6 @@
 import { Day09Input } from "./input";
 
-const lines = Day09Input;
-
-const grid = lines.map((line) => {
+const grid = Day09Input.map((line) => {
   const digits = line.split("").map(Number);
   return digits;
 });
@@ -52,15 +50,53 @@ const isVisible = (x: number, y: number) => {
   return false;
 };
 
+const getArrayScore = (myHeight: number, values: number[]) => {
+  let distance = 0;
+  while (true) {
+    const value = values.pop();
+    if (value === undefined) {
+      return distance;
+    }
+
+    ++distance;
+
+    if (value >= myHeight) {
+      return distance;
+    }
+  }
+};
+
+const getScenicScore = (x: number, y: number) => {
+  const myRow = grid[y];
+  const myHeight = myRow[x];
+  const left = myRow.slice(0, x);
+  const right = myRow.slice(x + 1).reverse();
+  const myColumn = columns[x];
+  const above = myColumn.slice(0, y);
+  const below = myColumn.slice(y + 1).reverse();
+
+  const leftScore = getArrayScore(myHeight, left);
+  const rightScore = getArrayScore(myHeight, right);
+  const aboveScore = getArrayScore(myHeight, above);
+  const belowScore = getArrayScore(myHeight, below);
+  const scenicScore = leftScore * rightScore * aboveScore * belowScore;
+
+  return scenicScore;
+};
+
 let totalVisible = 0;
+let highestScenicScore = 0;
 for (let y = 0; y < height; ++y) {
   for (let x = 0; x < width; ++x) {
     if (isVisible(x, y)) {
       totalVisible += 1;
     }
+
+    highestScenicScore = Math.max(highestScenicScore, getScenicScore(x, y));
   }
 }
 
 console.log({
   totalVisible,
+  highestScenicScore,
 });
