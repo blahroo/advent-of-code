d676935bb66543f41ec6b982ff353b6d1fe766e9
Blahroo <grahamhodgson1984@gmail.com>
2023-12-04 14:22:38 +0000

    day 1 part 1 correct solution

--- 2023/day01/index.ts
+++ 2023/day01/index.ts
@@ -1,3 +1,18 @@
 import { INPUT } from "./input";
 
 const ORIGINAL_LINES = INPUT.split("\n");
+
+console.log(ORIGINAL_LINES);
+const r = /\d/;
+const s = /(\d)(?!.*\d)/;
+let output = 0;
+
+for (let i = 0; i < ORIGINAL_LINES.length; i++) {
+  let firstDigit = ORIGINAL_LINES[i].match(r)[0];
+  let secondDigit = ORIGINAL_LINES[i].match(s)[0];
+  let finalNumber = parseInt(firstDigit + secondDigit);
+
+  output += finalNumber;
+}
+
+console.log(output);
